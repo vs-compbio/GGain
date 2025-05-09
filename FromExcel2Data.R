4a15a6144e863ed10308e9f97cdc0e857c1bd9ac
Singh <singhvis@oregonstate.edu>
2025-05-09 16:52:43 -0700

    full data elite trial : raw

--- FromExcel2Data.R
+++ FromExcel2Data.R
@@ -3,6 +3,8 @@ library(readxl)
 library(purrr)
 library(dplyr)
 
+setwd("C:/Users/singhvis/OneDrive - Oregon State University/GGainProject_24/GGain/SWELT/sweltFinal2/")
+
 # define path for the folder where all excel files are located
 fileLocation<-"."
 
@@ -25,12 +27,30 @@ df_allSheets<-map(excelFilesList, function(file){
 
 # Check the absence of a specific column name in the data frame list created 
 sapply(df_allSheets, function(df) {
-  if ("PREID" %in% names(df)) {
-    class(df[["PREID"]])
+  if ("YEAR" %in% names(df)) {
+    class(df[["YEAR"]])
   } else {
     NA_character_
   }
 })
+
+# row bind all dfs
+df_excels<-bind_rows(df_allSheets)
+
+# import csvs
+getwd()
+csvFilePath<-"./"
+csv_17to23<-list.files(csvFilePath, pattern = "\\.csv$", full.names = T)
+print(csv_17to23)
+
+csv_df_17to23<-lapply(csv_17to23, read.csv) %>% 
+  bind_rows()
+
+
+# verticaly join two DFs
+EliteData_full<-bind_rows(csv_df_17to23, df_excels)             ### Full data RAW without outlier removal
+
+
 
 
 
