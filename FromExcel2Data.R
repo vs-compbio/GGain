0a0ef143fe0d04bb781011b49f8020820beb62e3
Singh <singhvis@oregonstate.edu>
2025-05-09 15:10:27 -0700

    processing data directly frm excel files

---
+++ FromExcel2Data.R
@@ -0,0 +1,40 @@
+## merge SWELT data 
+library(readxl)
+library(purrr)
+library(dplyr)
+
+# define path for the folder where all excel files are located
+fileLocation<-"."
+
+# create a list of all excel files in the above file location
+excelFilesList<-list.files(fileLocation, pattern = "\\.xls[x]?$", full.names = T)
+
+# check file names in the above list
+print(excelFilesList)
+
+# list of dataframes from all sheets
+df_allSheets<-map(excelFilesList, function(file){
+  sheets <-excel_sheets(file)
+  map(sheets, function(sheet){
+    read_excel(path = file, sheet = sheet)
+    })
+}) %>% 
+  flatten()
+
+
+# Check the absence of a specific column name in the data frame list created 
+sapply(df_allSheets, function(df) {
+  if ("PREID" %in% names(df)) {
+    class(df[["PREID"]])
+  } else {
+    NA_character_
+  }
+})
+
+
+
+
+
+#lapply(df_allSheets, str)
+
+#big_union <- bind_rows(df_allSheets)
