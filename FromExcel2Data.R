a3447c81cba1a15fd83cce20d90a25ff9e917fdd
Singh <singhvis@oregonstate.edu>
2025-05-13 17:17:34 -0700

    added outlier detection method

--- FromExcel2Data.R
+++ FromExcel2Data.R
@@ -2,6 +2,9 @@
 library(readxl)
 library(purrr)
 library(dplyr)
+install.packages("ggblend")
+library(ggblend)
+
 
 setwd("C:/Users/singhvis/OneDrive - Oregon State University/GGainProject_24/GGain/SWELT/sweltFinal2/")
 
@@ -34,10 +37,10 @@ sapply(df_allSheets, function(df) {
   }
 })
 
-# row bind all dfs
+# row bind all dfs (1997-2016)
 df_excels<-bind_rows(df_allSheets)
 
-# import csvs
+# import CSVs (data from 2017-2023)
 getwd()
 csvFilePath<-"./"
 csv_17to23<-list.files(csvFilePath, pattern = "\\.csv$", full.names = T)
@@ -49,9 +52,30 @@ csv_df_17to23<-lapply(csv_17to23, read.csv) %>%
 
 # verticaly join two DFs
 EliteData_full<-bind_rows(csv_df_17to23, df_excels)             ### Full data RAW without outlier removal
+# remove data where the yield value is less than 1
+EliteData_full <- EliteData_full%>% 
+  filter(!(YIELD < 1))
 
+##########################################################################################
 
+####### Clean the Elite trial data (tukeys )
 
+# tukey's fences test
+
+tukey_outliers_elite_k3 <- EliteData_full %>%
+  group_by(LOCATION, YEAR) %>%
+  mutate(
+    Q1 = quantile(YIELD, 0.25),
+    Q3 = quantile(YIELD, 0.75),
+    IQR = Q3 - Q1,
+    lower_fence = Q1 - 3 * IQR,
+    upper_fence = Q3 +  3* IQR,
+    is_outlier = ifelse(YIELD < lower_fence | YIELD > upper_fence, TRUE, FALSE)
+  ) %>%
+  filter(is_outlier == TRUE)  # Keep only outliers for inspection
+
+getwd()
+write.csv(tukey_outliers_elite_k3, "tukeys_outliers_Elite_k3.csv")
 
 
 
