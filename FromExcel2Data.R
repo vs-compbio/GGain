d84f982f443112022aea05aa3926089e405cdd59
Singh <singhvis@oregonstate.edu>
2025-05-15 12:00:28 -0700

    heritability estimation using Cullis_H2

--- FromExcel2Data.R
+++ FromExcel2Data.R
@@ -2,9 +2,15 @@
 library(readxl)
 library(purrr)
 library(dplyr)
-install.packages("ggblend")
+# install.packages("ggblend")
 library(ggblend)
-
+# install.packages("EnvStats")  
+library(EnvStats)
+library(purrr)
+install.packages("remotes")
+remotes::install_github("etnite/bwardr", force = TRUE)
+library(bwardr)
+library(lme4)
 
 setwd("C:/Users/singhvis/OneDrive - Oregon State University/GGainProject_24/GGain/SWELT/sweltFinal2/")
 
@@ -60,7 +66,7 @@ EliteData_full <- EliteData_full%>%
 
 ####### Clean the Elite trial data (tukeys )
 
-# tukey's fences test
+# tukey's fences test.........................................Method 1
 
 tukey_outliers_elite_k3 <- EliteData_full %>%
   group_by(LOCATION, YEAR) %>%
@@ -77,5 +83,69 @@ tukey_outliers_elite_k3 <- EliteData_full %>%
 getwd()
 write.csv(tukey_outliers_elite_k3, "tukeys_outliers_Elite_k3.csv")
 
+# Rosner's test........................................................Method 2
 
+eliteOut_ros2 <- EliteData_full %>%
+  group_by(LOCATION, YEAR) %>%
+  summarise(
+    #rosner_result <- rosnerTest(nonNormFuldata$YIELD, k = 30)
+    rosner_outliers = list(rosnerTest(YIELD, k = 10)),  # Adjust 'k' for max number of outliers
+    .groups = "drop"
+  )
+
+# Extract results
+results_Ros_elite <- eliteOut_ros2 %>%
+  mutate(
+    summary = map(rosner_outliers, ~ .x$all.stats),  # Detailed results
+    significant_outliers = map(rosner_outliers, ~ .x$all.stats[.x$all.stats$Outlier == TRUE, ])
+  )
+
+
+# Unnest outliers
+all_outliers_Ros_elite <- results_Ros_elite %>%
+  select(LOCATION, YEAR, significant_outliers) %>%
+  unnest(cols = c(significant_outliers))
+write.csv(all_outliers_Ros_elite, "rosner_outlier_elite.csv")
+
+###########################################################################################
+
+## Estimate heritability of each trial and make a list
+
+
+
+# Define model
+modelFormula<- YIELD ~ (1|PREID) + (1|BLOC) 
+
+geno_term     <- "PREID"
+# Define a function to make things easier
+
+H2_eachtrial <-function(data_provided, Yr, Loc){
+  result<-tryCatch({
+    m1 <-lmer(modelFormula , data = data_provided)
+    H2ResCullis <-Cullis_H2(m1, geno_label = geno_term)$H2
+    tibble(TrailHeritability= H2ResCullis)
+  }, error = function(e) tibble(TrailHeritability = NA_real_))
+  
+  result %>% mutate(Year = Yr, Location = Loc, .before=1)
+}
+
+
+
+## estimate hetibility of each trial
+
+heritability_elteTrials_clean <-clean_elite2 %>%
+  group_by(YEAR, LOCATION) %>% 
+  group_modify(~H2_eachtrial(.x, .y$YEAR, .y$LOCATION)) %>% 
+  ungroup()
+
+
+#### Remove low heritability trials
+# try to remove traisl with no values and then run heritabilty again
+clean_elite <- EliteData_full %>%                                # your original data
+  group_by(YEAR, LOCATION) %>%                        # each trial = 1 group
+  filter(any(!is.na(YIELD) & YIELD != "")) %>%        # keep only if ≥1 real YIELD
+  ungroup()
+
+clean_elite2<-clean_elite %>% 
+  filter(!(YEAR ==2008))
 
