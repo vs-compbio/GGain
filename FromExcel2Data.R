7a363d2e86d3901b460c93b77a367de0876cc631
Singh <singhvis@oregonstate.edu>
2025-05-16 18:50:17 -0700

    updated with making different plots with elite data

--- FromExcel2Data.R
+++ FromExcel2Data.R
@@ -107,14 +107,14 @@ all_outliers_Ros_elite <- results_Ros_elite %>%
   unnest(cols = c(significant_outliers))
 write.csv(all_outliers_Ros_elite, "rosner_outlier_elite.csv")
 
-###########################################################################################
+######################################################################################################
 
 ## Estimate heritability of each trial and make a list
 
 
 
 # Define model
-modelFormula<- YIELD ~ (1|PREID) + (1|BLOC) 
+modelFormula<- YIELD ~ (1|PREID)  
 
 geno_term     <- "PREID"
 # Define a function to make things easier
@@ -129,15 +129,47 @@ H2_eachtrial <-function(data_provided, Yr, Loc){
   result %>% mutate(Year = Yr, Location = Loc, .before=1)
 }
 
-
-
 ## estimate hetibility of each trial
 
-heritability_elteTrials_clean <-clean_elite2 %>%
+heritability_elteTrials_clean3 <-clean_elite3 %>%
   group_by(YEAR, LOCATION) %>% 
   group_modify(~H2_eachtrial(.x, .y$YEAR, .y$LOCATION)) %>% 
   ungroup()
 
+heritability_elteTrials_clean4 <-clean_elite4 %>%
+  group_by(YEAR, LOCATION) %>% 
+  group_modify(~H2_eachtrial(.x, .y$YEAR, .y$LOCATION)) %>% 
+  ungroup()
+
+
+# ### heritbility of troubling trials
+# # Corvallis 2023
+# cor23<-clean_elite4 %>% 
+#   filter(YEAR == 2023 & LOCATION == "Corvallis")
+# 
+# heritability_cor23 <-cor23 %>%
+#   group_by(YEAR, LOCATION) %>% 
+#   group_modify(~H2_eachtrial(.x, .y$YEAR, .y$LOCATION)) %>% 
+#   ungroup()
+# 
+# mo1 <-lmer(modelFormula , data = cor23)
+# H2ResCullis <-Cullis_H2(mo1, geno_label = geno_term)$H2
+#######################################################################################################
+# correct the labels 
+EliteData_full <-EliteData_full %>% 
+  mutate(
+    PREID = recode(PREID,
+                   "ARS99124" ="ARS99123",
+                   "ARS99125" ="ARS99123",
+                   "BRUNDAGE97" ="BRUNDAGE96",
+                   "BRUNDAGE98" ="BRUNDAGE96",
+                   "IDO621" ="IDO620",
+                   "IDO622" ="IDO620",
+                   "WESTBRD529" ="WESTBRD528",
+                   "WESTBRD530" ="WESTBRD528",
+                   )
+  )
+  
 
 #### Remove low heritability trials
 # try to remove traisl with no values and then run heritabilty again
@@ -146,6 +178,156 @@ clean_elite <- EliteData_full %>%                                # your original
   filter(any(!is.na(YIELD) & YIELD != "")) %>%        # keep only if ≥1 real YIELD
   ungroup()
 
-clean_elite2<-clean_elite %>% 
-  filter(!(YEAR ==2008))
+# Removing the year 2008 because it doesn't has info on PREID
+
+clean_elite2 <-clean_elite %>% 
+  filter(!(YEAR == 2008))
+
+
+# remove less tha one rep geno
+# find out which PREIDs have just one rep
+rep_table_elite <- clean_elite2 %>%                       
+  group_by(YEAR, LOCATION, PREID) %>%      
+  summarise(
+    n_reps = n(),                             # row-count = # of replicates
+    .groups = "drop"                          # return an un-grouped tibble
+  )
+
+# remove single rep PREIDs
+clean_elite3 <- clean_elite2 %>%            # your full data frame
+  group_by(YEAR, LOCATION, PREID) %>%       # one group = one trial × genotype
+  filter(n() > 1) %>%                       # keep only groups with ≥ 2 rows
+  ungroup()
+
+
+#####################################################################################
+## plot trial means of trials
+# trial means
+trialMeans <- clean_elite3 %>%            # your cleaned data set
+  group_by(YEAR, LOCATION) %>%             # one group = one trial
+  summarise(
+    trialMeans = mean(YIELD, na.rm = TRUE),# mean across all plots in that trial
+    .groups    = "drop"
+  )
+
+# remove Lexington 2023 data due to abnormally low yield
+clean_elite3 <-clean_elite3 %>% 
+  filter(!(YEAR ==2023 & LOCATION == "Lexington"))
+# remove Dufur, Madsen, SouthValley
+clean_elite3 <-clean_elite3 %>% 
+  filter(!(LOCATION == "Dufur"|LOCATION == "Madsen"|LOCATION == "SouthValley" ))
+
+# Correct the location names 
+clean_elite3 <-clean_elite3 %>% 
+  mutate(
+    LOCATION = recode(LOCATION,
+                      "ARLINGTON" = "Arlington",
+                      "CONDON" = "Condon",
+                      "CORVALLIS" = "Corvallis",
+                      "HERMISTON" = "Hermiston",
+                      "KASEBERG" = "Kaseberg",
+                      "Kfall" = "Kfalls",
+                      "La Grande" = "LaGrande",
+                      "LAGRANDE" = "LaGrande",
+                      "LEXINGTON" = "Lexington",
+                      "MADRAS" = "Madras",
+                      "MFH2O" = "MiltonFreewater",
+                      "Milton-Freewater" = "MiltonFreewater",
+                      "Milton Freewater" = "MiltonFreewater",
+                      "MORO" = "Moro",
+                      "N. Valley" = "NorthValley",
+                      "N.Valley" = "NorthValley",
+                      "N.VALLEY" = "NorthValley",
+                      "North Valley" = "NorthValley",
+                      "NV" = "NorthValley",
+                      "ONTARIO" = "Ontario",
+                      "PENDLETON" = "Pendleton",
+                      "Hermison" = "Hermiston"
+      
+    )
+  )
+
+###############################################################################
+###  Clean the PREIDs
+# find unique PREIDs
+uniquePREIDs<-clean_elite4 %>%
+  distinct(PREID)
+  
+### Clean PREID
+library(stringr)
+
+clean_elite3 <-clean_elite3 %>% 
+  mutate(
+    PREID = str_replace(PREID, "^OR\\s+", "OR")   # delete space(s) after “OR”
+  )
+  
+  
+  
+clean_elite4 <-clean_elite3 %>% 
+  mutate(
+    PREID = recode(PREID,
+                      "ARSC96059-1*" = "ARSC96059-1",
+                      "ARS97135-9*" = "ARS97135-9",
+                      "BRUNDAGE96" = "Brundage 96",
+                      "BRUNDAGE 96" = "Brundage 96",
+                      "CHUKAR" = "Chukar",
+                      "BZ 6W99-456*" = "BZ 6W99-456",
+                      "CODA" = "Coda",
+                      "DUNE" = "Dune",
+                      "FINCH" = "Finch",
+                      "GENE" = "Gene",
+                      "MFH2O" = "MiltonFreewater",
+                      "ID99-419*" = "ID99-419",
+                      "ID99-435*" = "ID99-435",
+                      "MADSEN" = "Madsen",
+                      "MASAMI" = "Masami",
+                      "MOHLER" = "Mohler",
+                      "OR2020787*" = "OR2020787",
+                      "OR2030238*" = "OR2030238",
+                      "OR2030239*" = "OR2030239",
+                      "OR2030411*" = "OR2030411",
+                      "OR2030554*" = "OR2030554",
+                      "ORH010837*" = "ORH010837",
+                   "ORI2042037*" = "ORI2042037",
+                   "ROD" = "Rod",
+                   
+                   "Rod/Tubbs06" = "Rod&Tubbs06",
+                   "SIMON" = "Simon",
+                   "STEPHENS" = "Stephens",
+                   "TUBBS" = "Tubbs",
+                   "TUBBS-06" = "Tubbs-06",
+                   "TUBBS-06*" = "Tubbs-06",
+                   "UI Magic" = "UI-Magic",
+                   "WA8134" = "WA 8134",
+                   "WEATHERFORD" = "Weatherford",
+                   "WESTBRED 528" = "Westbred 528",
+                   "WESTBRD528" = "Westbred 528",
+                   "ORCF-101" = "ORCF 101",
+                   "ORCF-102" = "ORCF 102"
+                      
+    )
+  )
+
+# find unique PREIDs
+uniquePREIDs_2<-clean_elite4 %>%
+  distinct(PREID)
+write.csv(uniquePREIDs_2, "uniquePREIDEliteFinal.csv")
+
+########################################################################################
+#Remove trials with heritabilit values less than 0.2
+
+low_h2<-heritability_elteTrials_clean4 %>% 
+  filter(is.finite(TrailHeritability) & TrailHeritability <0.2) %>% 
+  select(YEAR, LOCATION)
+         
+clean_elite5 <- clean_elite4 %>% 
+  anti_join(low_h2, by = c("YEAR", "LOCATION"))
+
+
+#### save the clean and final data
+write.csv(clean_elite5, "clean_elite5_Final_May15.csv")
+  
+#############################################################################
+
+
 
--- GGain_elite.R
+++ GGain_elite.R
@@ -17,8 +17,6 @@ dataElite<-read.csv("clean_elite5_Final_May15.csv")
 ################################################################################## Plot trial distro: 
 #                                               upto line 126
 ## 1. datagrid and locplot :elite
-dataGrid<-dataElite %>% 
-  complete(LOCATION, YEAR)
 
 df_counts <- dataElite %>%                     # your raw observations
   group_by(LOCATION, YEAR) %>%          # one group = one trial
@@ -174,9 +172,135 @@ ggplot(plot_df,
 ggsave("checkDistrobutionElite_May16.tiff", width = 16, height = 10, units = "in", dpi = 600)
 
 ###################################################################################################
+########## Shared vs unique entries
+df_uniqueGenYr <- dataElite %>%                       # <- your multiyear, multilocation data
+  distinct(PREID, YEAR)    
+library(tibble) 
+incidenceMatr <- df_uniqueGenYr %>%
+  mutate(present = 1) %>%                    # marker column
+  pivot_wider(names_from = YEAR,
+              values_from = present,
+              values_fill = 0) %>%           # missing → 0
+  column_to_rownames("PREID") %>%
+  as.matrix()  
 
+
+year_Matr <- t(incidenceMatr) %*% incidenceMatr
+yrs <- sort(unique(df_uniqueGenYr$YEAR))
+year_Matr <- year_Matr[ as.character(yrs), as.character(yrs) ]
+# heatmap
+df_htmp<-as.data.frame(as.table(year_Matr))
+
+ggplot(df_htmp, aes(x = Var1, y = Var2, fill = Freq)) +
+  geom_tile(colour = "white", linewidth = 0.25) +
+  # ---- Optional: print the counts in each cell ------------------------------
+  geom_text(aes(label = Freq), colour = "black", size = 5.5) +
+  scale_fill_gradientn(
+    colours   = viridis(256, option = "cividis", direction = -1),
+    name      = "# Genotypes\n(Freq)",  # legend title
+    limits    = c(0, max(df_htmp$Freq)), # keeps the colour bar consistent
+    na.value  = "grey90"
+  ) +
+  labs(x = "Year", y = "Year") +
+  scale_x_discrete(expand = c(0, 0), position = "top") +  # put x-axis on top
+  scale_y_discrete(expand = c(0, 0)) +
+  coord_fixed() +                                         # square cells
+  theme_minimal(base_size = 12) +
+  theme(
+    axis.text.x  = element_text(size = 16,angle = 90, hjust = 0, vjust = 0.5, colour = "black"),
+    axis.text.y  = element_text(size = 16,angle = 0, hjust = 0, vjust = 0.5, colour = "black"),
+    axis.title.x  = element_text(margin = margin(t = 18),size = 20,colour = "black"),
+    axis.title.y  = element_text(margin = margin(r = 12),size = 20, colour = "black"),
+    panel.grid   = element_blank()
+  )
+ggsave("Common&UniqueEntries_Elite_May16.tiff", width = 12, height = 12, units = "in", dpi = 600)
+
+#####################################################################################################
+##############      Heatmap or lineplot for heritability
+ggplot(heritability_elteTrials_clean4, aes(x = LOCATION, y = factor(YEAR), fill = TrailHeritability)) +
+  geom_tile(colour = "white", linewidth = 0.2) +
+  scale_fill_viridis_c(name = "Heritability", option = "plasma") +
+  labs(x = "Location", y = "Year") +
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_y_discrete(expand = c(0, 0)) +
+  coord_fixed() +
+  theme_minimal(base_size = 12) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+# regression lines
+# only selected location
+library(broom)
+heritability_elteTrials_clean4SEL<-heritability_elteTrials_clean4 %>% 
+  filter(LOCATION %in% c("Corvallis", "Moro", "Pendleton", "Kaseberg", "Lexington", "Hermiston"))
+
+# Ls for each loc, slope, and 95% CI
+slopes <- heritability_elteTrials_clean4SEL %>%                     
+  group_by(LOCATION) %>%
+  summarise(beta = coef(lm(TrailHeritability ~ YEAR))[2],    
+            .groups = "drop")
+
+
+annot_df <- heritability_elteTrials_clean4SEL %>%                   # LOCATION, YEAR, h2
+  group_by(LOCATION) %>%
+  slice_min(order_by = YEAR, n = 1) %>%       # earliest year instead of latest
+  left_join(slopes, by = "LOCATION") %>%
+  mutate(
+    label = sprintf("β = %.3f", beta),        # slope only
+    YEAR  = YEAR - 0.3                        # nudge a bit left of the first point
+  )
+
+
+## Line plot of heritability with year (regression)
+ggplot(heritability_elteTrials_clean4SEL, aes(x = YEAR,
+                        y = TrailHeritability,
+                        colour = LOCATION)) +
+  geom_point(size = 2, alpha = .7) +           # the raw points
+  geom_smooth(method = "lm",                   #   + straight-line fit
+              se = F,  
+              level  = 0.9,
+              alpha  = 0.01,
+              linewidth = 2) +                 # line thickness
+  scale_colour_viridis_d(name = "Location",option = "mako",begin  = 0.15,          # skip the first 15 %  (lightest)
+                         end    = .90) +
+  labs(x = "Year",
+       y = "Heritability (H²)") +
+   geom_text(data = annot_df,
+            aes(x = YEAR, y = TrailHeritability, label = label, colour = LOCATION),
+            hjust = 0,            # left-align to the x position
+            vjust = 0.5,
+            size  = 6,            # ≈ 8 pt; shrink if the plot is crowded
+            show.legend = T) +
+  theme(
+    axis.text.x  = element_text(size = 18,angle = 0, hjust = 0.5, vjust = 1, colour = "black"),
+    axis.text.y  = element_text(size = 18,angle = 0, hjust = 0, vjust = 0.5, colour = "black"),
+    axis.title.x  = element_text(margin = margin(t = 18),size = 20,colour = "black"),
+    axis.title.y  = element_text(margin = margin(r = 12),size = 20, colour = "black"),
+    panel.grid   = element_blank()
+  ) +
+  theme(
+    legend.title = element_text(size = 16, face = "bold"),  # title font
+    legend.text  = element_text(size = 16)                  # entry labels
+  )
+
+ggsave("Heritability_RegressionTime_Elite_May16.tiff", width = 10, height = 8, units = "in", dpi = 600)
+
+
+###################################################################################################################
 ####################  Fit models overall and location-wise
+library(asreml)
 
-
-
+dataElite_ggF<-dataElite %>% 
+  mutate(
+    across(
+      c(YEAR, PREID, LOCATION), as.factor
+    )
+  )
+str(dataElite)
+model_full_elite <- asreml(fixed = YIELD ~ YEAR + PREID,
+                           random = ~ LOCATION + YEAR:LOCATION +
+                             YEAR:PREID + LOCATION:PREID,
+                           residual = ~units,
+                           data = dataElite_ggF)
+predictions_full_elite <- predict(model_full_elite, classify = "PREID")
 
